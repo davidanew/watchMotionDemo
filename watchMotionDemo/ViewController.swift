fd8e78af89efd4eddc06e1384b25e4246c373f8a
davidanew <davidanew@googlemail.com>
2019-04-18 17:01:53 +0100

    Initial Commit

---
+++ watchMotionDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  watchMotionDemo
+//
+//  Created by David New on 18/04/2019.
+//  Copyright © 2019 David New. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
