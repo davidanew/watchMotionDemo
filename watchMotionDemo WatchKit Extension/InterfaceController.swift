7e8fe14c9063f9107f1bb6e0648c104a56decb0e
davidanew <davidanew@googlemail.com>
2019-04-20 09:44:50 +0100

    Added comments

--- watchMotionDemo WatchKit Extension/InterfaceController.swift
+++ watchMotionDemo WatchKit Extension/InterfaceController.swift
@@ -10,15 +10,24 @@ import os.log
 class InterfaceController: WKInterfaceController, WCSessionDelegate {
     
     @IBOutlet weak var accLabel: WKInterfaceLabel!
+    //Handles motion from the watch's sensors
     let motionManager = CMMotionManager()
+    //A workout session is needed so we can get readings in the background
     var workoutSession : HKWorkoutSession?
+    //Heath store only needed for workout initialisation
     let healthStore = HKHealthStore()
+    //For sending messages to the phone
     var connectivitySession : WCSession?
     
+    //Start button has been pressed
+    //TODO: Update colours on buttons on start and stop
     @IBAction func startButton() {
+        //If this is nil then there is no workout session running
+        //Could be a guard statement
         if (workoutSession != nil){
             return
         }
+        //Create the workout configuration and start the session
         let workoutConfiguration = HKWorkoutConfiguration()
         workoutConfiguration.activityType = .mixedCardio
         workoutConfiguration.locationType = .indoor
@@ -26,12 +35,16 @@ class InterfaceController: WKInterfaceController, WCSessionDelegate {
             workoutSession = try HKWorkoutSession(healthStore: healthStore, configuration: workoutConfiguration)
         }
         catch {
-            print("Error generating workout session")
+            os_log("Error generating workout session")
             return
         }
+        //Start the workout session
         workoutSession?.startActivity(with: Date())
         if motionManager.isDeviceMotionAvailable {
+            //TODO: Update this for max counting
+            //request updates from motion manager
             motionManager.startDeviceMotionUpdates(to: OperationQueue.current!) { (data, error) in
+                //Need valid data for acceleration and rortation
                 guard let acceleration : CMAcceleration = data?.userAcceleration else {
                     os_log("Invalid acceleration")
                     return
@@ -40,8 +53,10 @@ class InterfaceController: WKInterfaceController, WCSessionDelegate {
                     os_log("Invalid rotation rate")
                     return
                 }
+                //Display total acceleration on watch
                 let totalAcceleration = sqrt(pow(acceleration.x, 2) + pow(acceleration.y, 2) + pow(acceleration.z, 2))
                 self.accLabel.setText(String(format: "%.1fg", totalAcceleration))
+                //Create a dictionary of the acceleration and rotation data
                 let messageDict : [String : Double] = [
                     "AccX" : acceleration.x,
                     "AccY" : acceleration.y,
@@ -50,6 +65,7 @@ class InterfaceController: WKInterfaceController, WCSessionDelegate {
                     "RotY" : rotationRate.y,
                     "RotZ" : rotationRate.z
                 ]
+                //Send this dictionary to the watch
                 self.connectivitySession?.sendMessage(messageDict, replyHandler: nil , errorHandler: { (error) in
                     print("message error \(error)")
                 })
@@ -60,20 +76,27 @@ class InterfaceController: WKInterfaceController, WCSessionDelegate {
         }
     }
     @IBAction func stopButton() {
+        //If workout session is already nil we don't need to do anaything
+        //TODO: Think a bout guard instead
         if workoutSession == nil {
             return
         }
+        //Stop the motion update processing
         motionManager.stopDeviceMotionUpdates()
+        //Remove the workout session
         workoutSession?.end()
         workoutSession = nil
+        //Upadate the data label
         self.accLabel.setText("Stopped")
     }
     
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
+        //Sensor data update frequency
         motionManager.deviceMotionUpdateInterval = 0.1
+        //Show user the workout is not yet running
         self.accLabel.setText("Stopped")
-
+        //Start phone connection session
         if WCSession.isSupported() {
             connectivitySession = WCSession.default
             connectivitySession?.delegate = self
@@ -92,8 +115,9 @@ class InterfaceController: WKInterfaceController, WCSessionDelegate {
         // This method is called when watch view controller is no longer visible
         super.didDeactivate()
     }
+    
+    //Required WCSessionDelegate
     func session(_ session: WCSession, activationDidCompleteWith activationState: WCSessionActivationState, error: Error?) {
         os_log("WC session activated on watch")
     }
-
 }
--- watchMotionDemo/ViewController.swift
+++ watchMotionDemo/ViewController.swift
@@ -1,22 +1,16 @@
-//
-//  ViewController.swift
-//  watchMotionDemo
-//
-//  Created by David New on 18/04/2019.
 //  Copyright © 2019 David New. All rights reserved.
-//
 
 import UIKit
 import WatchConnectivity
 import os.log
 
-
-
 class ViewController: UIViewController, WCSessionDelegate  {
     
+    //Sensor values are multiplied by this to produce graphs with the right range
     let accGain : Double = 1.0
     let rotGain : Double = 0.1
     
+    //Graphs for senso values use progress views
     @IBOutlet weak var progressAX: UIProgressView!
     @IBOutlet weak var progressAY: UIProgressView!
     @IBOutlet weak var progressAZ: UIProgressView!
@@ -24,36 +18,33 @@ class ViewController: UIViewController, WCSessionDelegate  {
     @IBOutlet weak var progressRY: UIProgressView!
     @IBOutlet weak var progressRZ: UIProgressView!
     
+    //Label for debugging/logging
+    @IBOutlet weak var testLabel: UILabel!
+
+    //TODO: Either remove this or figure out how it is meant to work
     struct Log {
         static var general = OSLog(subsystem: "com.watchMotionDemo", category: "general")
     }
     
+    //Session for recieving data from watch
     var connectivitySession : WCSession?
+    
+    //Reuquired WCSessionDelegate methods
 
     func session(_ session: WCSession, activationDidCompleteWith activationState: WCSessionActivationState, error: Error?) {
         os_log("activationDidComplete on phone")
     }
-    
     func sessionDidBecomeInactive(_ session: WCSession) {
         os_log("sessionDidBecomeInactive on phone")
 
     }
-    
     func sessionDidDeactivate(_ session: WCSession) {
         os_log("sessionDidDeactivate on phone")
-
     }
     
+    //handle message from watch
     func session(_ session: WCSession, didReceiveMessage message: [String : Any]) {
- /*
-        if let response = message["AccX"] as? Double {
-            os_log("phone message recieved")
-            DispatchQueue.main.async {
-                self.testLabel.text = "phone message recieved : \(response)"
-            }
-             
-        }
- */
+        //Extract values from message dictionary
         let accX : Double = message["AccX"] as? Double ?? 0.0
         let accY : Double = message["AccY"] as? Double ?? 0.0
         let accZ : Double = message["AccZ"] as? Double ?? 0.0
@@ -61,6 +52,7 @@ class ViewController: UIViewController, WCSessionDelegate  {
         let rotY : Double = message["RotY"] as? Double ?? 0.0
         let rotZ : Double = message["RotZ"] as? Double ?? 0.0
         DispatchQueue.main.async {
+            //Update graphs
             self.testLabel.text = (String(accX))
             self.progressAX.progress = Float(accX * self.accGain)
             self.progressAY.progress = Float(accY * self.accGain)
@@ -72,15 +64,11 @@ class ViewController: UIViewController, WCSessionDelegate  {
 
     }
 
-    @IBOutlet weak var testLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        //testLabel.text = "phone view did load"
-        //print("phone view did load")
-        //os_log("watchMotionDemo phone view did load", log: Log.general, type: .info)
         os_log("viewDidLoad")
-        
-        // Do any additional setup after loading the view.
+        //Start watch connection to listen for messages
         if WCSession.isSupported() {
             connectivitySession = WCSession.default
             connectivitySession?.delegate = self
